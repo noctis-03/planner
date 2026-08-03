c0c952189f4310b650ef5544e05d7adfbd173da2
Kim Gaon <gaonkim12345@gmail.com>
2026-08-03 11:43:05 +0900

    Delete lib directory

--- lib/widgets/drag_payload.dart
+++
@@ -1,9 +0,0 @@
-/// 드래그되는 블록의 정보
-class DragPayload {
-  final String blockId;
-
-  /// true 면 복사, false 면 이동 (기본값: 이동)
-  final bool copy;
-
-  const DragPayload(this.blockId, {this.copy = false});
-}
